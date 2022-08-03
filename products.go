c2a1b91b3b16f6130eeae18bec14c5fc42ee2f93
DudinDmitriy <dood_pvs@mail.ru>
2022-08-03 19:43:29 +0300

    111

---
+++ products.go
@@ -0,0 +1,25 @@
+package products
+
+import "sync"
+
+type ProductID int
+
+type CategorieID int
+
+type Product struct {
+	ID         ProductID
+	Name       string
+	categories []CategorieID
+}
+
+type ListProduct struct {
+	list map[ProductID]Product
+}
+
+func (lp *ListProduct) Init() {
+
+}
+
+func (lp *ListProduct) GetListJSON() {
+
+}
